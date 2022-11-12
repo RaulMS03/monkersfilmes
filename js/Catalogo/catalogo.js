e5e44088c81cbbd93ea1e9ee220e8b70ab1331b0
ericcarvlh <ericcarvalho935@gmail.com>
2022-11-12 20:52:17 -0300

    implementação da API na pagina de catalogo (inicio)

--- js/Catalogo/catalogo.js
+++ js/Catalogo/catalogo.js
@@ -34,7 +34,7 @@ addEventListener('load', () => {
             let idFilme = data[index]['id'];
             let posterFilme = `https://image.tmdb.org/t/p/w500${data[index]['poster_path']}`;
 
-            imgElement = document.getElementById(`filme-${index+1}`);
+            let imgElement = document.getElementById(`filme-${index+1}`);
             imgElement.src = posterFilme;
             imgElement.alt = idFilme;
             
@@ -53,7 +53,7 @@ addEventListener('load', () => {
             let idSerie = data[index]['id'];
             let posterSerie = `https://image.tmdb.org/t/p/w500${data[index]['poster_path']}`;
 
-            imgElement = document.getElementById(`serie-${index+1}`);
+            let imgElement = document.getElementById(`serie-${index+1}`);
             imgElement.src = posterSerie;
             imgElement.alt = idSerie;
             
@@ -61,6 +61,80 @@ addEventListener('load', () => {
         }    
     });
 
+    mediaType = [
+        'movie',
+        'tv'
+    ];
+
+    // gera um indice randomico entre 0 e 1.
+    // se for 0 entao sera listado os filmes, se for 1 sera listado as series
+    data = consultaPorGenero(mediaType[Math.floor(Math.random() * 2)], '28');
+
+    data.then( promise => {
+        data = promise['results'].slice(0, 4);
+        
+        let index = 0;
+        while(index < data.length){
+            let idEntreterimento = data[index]['id'];
+            let posterEntreterimento = `https://image.tmdb.org/t/p/w500${data[index]['poster_path']}`;
+
+            let imgElement = document.getElementById(`acao-${index+1}`);
+            imgElement.src = posterEntreterimento;
+            imgElement.alt = idEntreterimento;
+            
+            index++;
+        }    
+    });
+
+    data = consultaAtualizacoesDaSemana();
+
+    data.then( promise => {
+        data = promise['results'].slice(0, 4);
+        
+        let index = 0;
+        let indexId = 0;
+        while(index < data.length){
+            let idEntreterimento = data[index]['id'];
+            let posterEntreterimento;
+            let dadosFilme = consultaDadosPorId('movie', idEntreterimento);
+
+            dadosFilme.then(promise => {
+                return promise;
+            }).then(jsonData => {
+                posterEntreterimento = jsonData['poster_path']; 
+            }).then(() => {
+                let imgElement = document.getElementById(`atualizacao-${indexId+1}`);
+                imgElement.alt = idEntreterimento;
+                if(posterEntreterimento){
+                    imgElement.src = `https://image.tmdb.org/t/p/w500${posterEntreterimento}`;
+                } else {
+                    imgElement.className = 'img-nao-encontrada';
+                    imgElement.src = 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg';
+                }
+                indexId++;
+            })
+
+            index++;
+        }    
+    });
+
+    data = consultaPorGenero(mediaType[Math.floor(Math.random() * 2)], '27');
+
+    data.then( promise => {
+        data = promise['results'].slice(0, 4);
+        
+        let index = 0;
+        while(index < data.length){
+            let idEntreterimento = data[index]['id'];
+            let posterEntreterimento = `https://image.tmdb.org/t/p/w500${data[index]['poster_path']}`;
+
+            let imgElement = document.getElementById(`terror-${index+1}`);
+            imgElement.src = posterEntreterimento;
+            imgElement.alt = idEntreterimento;
+            
+            index++;
+        }    
+    });
 });
 
 // mediaType -> tipo de midia: all, movie, tv e person.
@@ -70,12 +144,68 @@ function consultaTendenciasTMDB(mediaType, timeWindow){
     const api_key = '0279131df8eec775fc20e8a5d97731f6';
     const language = 'pt-BR';
     const sort_by = 'popularity.desc';
+    const include_adult = 'false';
+    const include_video= 'false';
+    const page = '1';
     
-    return fetch(`${url}?api_key=${api_key}&language=${language}&sort_by=${sort_by}`, {
+    return fetch(`${url}?api_key=${api_key}&language=${language}&sort_by=${sort_by}&include_adult=${include_adult}&include_video=${include_video}&page=${page}`, {
                 method: "GET",
             }).then(response => { 
                 return response.json();
             }).then(data => {
-                return data
+                return data;
             });
-}   
+}
+
+// mediaType -> tipo de midia: all, movie, tv e person.
+// genreId -> id do genero.
+function consultaPorGenero(mediaType, genreId) {
+    const url = `https://api.themoviedb.org/3/discover/${mediaType}`;
+    const api_key = '0279131df8eec775fc20e8a5d97731f6';
+    const language = 'pt-BR';
+    const sort_by = 'popularity.desc';
+    const include_adult = 'false';
+    const include_video= 'false';
+    const page = '1';
+
+    return fetch(`${url}?api_key=${api_key}&language=${language}&sort_by=${sort_by}&include_adult=${include_adult}&include_video=${include_video}&genre_id=${genreId}&page=${page}`, {
+        method: "GET",
+    }).then(response => { 
+        return response.json();
+    }).then(data => {
+        return data;
+    });
+}
+
+function consultaAtualizacoesDaSemana() {
+    const url = 'https://api.themoviedb.org/3/movie/changes';
+    const api_key = '0279131df8eec775fc20e8a5d97731f6';
+    const language = 'pt-BR';
+    const sort_by = 'primary_release_date.desc';
+    const include_adult = 'false';
+
+    return fetch(`${url}?api_key=${api_key}&language=${language}&sort_by=${sort_by}&include_adult=${include_adult}`, {
+        method: "GET",
+    }).then(response => { 
+        return response.json();
+    }).then(data => {
+        return data;
+    });
+}
+
+// mediaType -> tipo de midia: all, movie, tv e person.
+// id -> id do filme ou serie em questão.
+function consultaDadosPorId(mediaType, id) {
+    const url = `https://api.themoviedb.org/3/${mediaType}/${id}`;
+    const api_key = '0279131df8eec775fc20e8a5d97731f6';
+    const language = 'pt-BR';
+
+    return fetch(`${url}?api_key=${api_key}&language=${language}`, {
+        method: "GET",
+    }).then(response => {
+        return response.json()
+    }).then(data => {
+        return data;
+    })
+
+}
