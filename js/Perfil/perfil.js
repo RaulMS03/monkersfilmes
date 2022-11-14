1252eb8788892a98b8d092075096491befa4e673
ericcarvlh <ericcarvalho935@gmail.com>
2022-11-13 21:31:33 -0300

    alterações no catalogo e perfil

--- js/Perfil/perfil.js
+++ js/Perfil/perfil.js
@@ -6,7 +6,37 @@ addEventListener("load", () => {
         document.getElementById('nome-usuario').innerText = nomeUsuario;
         document.getElementById('img-foto-perfil').src = urlFotoPerfil;
 
+        data = consultaDadosFtosPerfil();
+        
+        data.then(promise => {
+            
+            let index = 0;
+            while(index < promise.length){
+
+                let imgPerfil = document.getElementById(`perfil-${index+1}`);
+                let fotoPerfil = promise[index]['url_foto_perfil'];
+
+                imgPerfil.src = fotoPerfil;
+
+                index++;
+            }
+
+        });
+
     } else {
         window.location.href = '../Login/login.html';
     }
 });
+
+function consultaDadosFtosPerfil(){
+    const url = 'http://api-monkers-entertainment.coolpage.biz/get/consultaFotosDePerfil.php';
+    
+    return fetch(url, {
+            method: "GET",
+        }).then(function(response) { 
+            return response.json();
+        }).then(function(data) {
+            if(data.response == "True")
+                return data.data;
+        });
+}
